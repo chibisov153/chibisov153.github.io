3860a9c6428afef39e47fe8fd0ae47a512048c6f
chibisov153 <53859711+chibisov153@users.noreply.github.com>
2019-08-09 19:29:52 +0700

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(function() {
+
+  $('.item').hover( function() {
+    //$(this).css("opacity", "1");
+    //$('.item').css("opacity", "0.5");
+  }, function(){
+    //$(this).css("opacity", "1");
+    //$('.item').css("opacity", "1");
+  })  
+
+});
+
+
+
+
