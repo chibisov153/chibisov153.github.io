b28acc5cd1bd2de20e6cd9a36e3b804db7a33b75
Dima Ch <chibisov153@gmail.com>
2022-02-19 19:58:10 +0700

    del 0.04

--- tours/nuxt/static/1645270909/tours/great-geysir/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftours\u002Fnuxt\u002Fstatic\u002F1645270909",layout:"Background",error:a,serverRendered:true,routePath:"\u002Ftours\u002Fgreat-geysir",config:{_app:{basePath:"\u002Ftours\u002F",assetsPath:"\u002Ftours\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"b35b3208"}}}}(null));
