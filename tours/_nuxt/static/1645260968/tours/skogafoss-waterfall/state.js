28e7aae40f00ebb0a99594a988e90877fe9f9549
Dima Ch <chibisov153@gmail.com>
2022-02-19 16:19:04 +0700

    del 0.2

--- tours/_nuxt/static/1645260968/tours/skogafoss-waterfall/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftours\u002F_nuxt\u002Fstatic\u002F1645260968",layout:"Background",error:a,serverRendered:true,routePath:"\u002Ftours\u002Fskogafoss-waterfall",config:{_app:{basePath:"\u002Ftours\u002F",assetsPath:"\u002Ftours\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ba937f48"}}}}(null));
