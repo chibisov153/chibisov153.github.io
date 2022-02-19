d7d6827d2f24ea796363f27cf0b087e50b030f1c
Dima Ch <chibisov153@gmail.com>
2022-02-19 16:39:00 +0700

    del 0.3

--- tours/_nuxt/static/1645262409/tours/hallgrimskirkja/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftours\u002F_nuxt\u002Fstatic\u002F1645262409",layout:"Background",error:a,serverRendered:true,routePath:"\u002Ftours\u002Fhallgrimskirkja",config:{_app:{basePath:"\u002Ftours\u002F",assetsPath:"\u002Ftours\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"30d7d71c"}}}}(null));
