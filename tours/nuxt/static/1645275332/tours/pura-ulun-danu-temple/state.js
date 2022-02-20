2919393546decfccaa2ce93706bbe71792c409f8
Dima Ch <chibisov153@gmail.com>
2022-02-20 10:13:30 +0700

    del 0.5

--- tours/nuxt/static/1645275332/tours/pura-ulun-danu-temple/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftours\u002Fnuxt\u002Fstatic\u002F1645275332",layout:"Background",error:a,serverRendered:true,routePath:"\u002Ftours\u002Fpura-ulun-danu-temple",config:{_app:{basePath:"\u002Ftours\u002F",assetsPath:"\u002Ftours\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"a89f737b"}}}}(null));
