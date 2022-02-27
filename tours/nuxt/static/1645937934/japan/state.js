14205a7c3428b51de8f68e32260837c0c0eca27f
Dima Ch <chibisov153@gmail.com>
2022-02-27 12:02:45 +0700

    up date 27.02.22 0.3

---
+++ tours/nuxt/static/1645937934/japan/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftours\u002Fnuxt\u002Fstatic\u002F1645937934",layout:"Background",error:a,serverRendered:true,routePath:"\u002Fjapan",config:{_app:{basePath:"\u002Ftours\u002F",assetsPath:"\u002Ftours\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"1375c406"}}}}(null));
