f8fbfcfc567019f46ac714bd8dd25a31cb9145e9
Dima Ch <chibisov153@gmail.com>
2022-02-27 12:36:19 +0700

    up 27.02.22 0.4

---
+++ tours/nuxt/static/1645939786/tours/garbage-island/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftours\u002Fnuxt\u002Fstatic\u002F1645939786",layout:"Background",error:a,serverRendered:true,routePath:"\u002Ftours\u002Fgarbage-island",config:{_app:{basePath:"\u002Ftours\u002F",assetsPath:"\u002Ftours\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"424929ee"}}}}(null));
