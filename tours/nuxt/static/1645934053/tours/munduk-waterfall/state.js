db719139f5887081340a5a7eb1b6edfa2be93b9b
Dima Ch <chibisov153@gmail.com>
2022-02-27 12:00:18 +0700

    dal 27.02.22 0.3

--- tours/nuxt/static/1645934053/tours/munduk-waterfall/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftours\u002Fnuxt\u002Fstatic\u002F1645934053",layout:"Background",error:a,serverRendered:true,routePath:"\u002Ftours\u002Fmunduk-waterfall",config:{_app:{basePath:"\u002Ftours\u002F",assetsPath:"\u002Ftours\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"a0b1c47c"}}}}(null));
