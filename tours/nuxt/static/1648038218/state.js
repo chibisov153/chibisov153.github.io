fcd1a4a17043ead0b0da4f999d0b06b223791b2e
Dima Ch <chibisov153@gmail.com>
2022-03-23 23:36:04 +0700

    delete 23.03.22-4

--- tours/nuxt/static/1648038218/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftours\u002Fnuxt\u002Fstatic\u002F1648038218",layout:"Background",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftours\u002F",assetsPath:"\u002Ftours\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"532582af"}}}}(null));
