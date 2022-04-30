17345948381e0be8ddd9221217c1bedae971f8ef
Dima Ch <chibisov153@gmail.com>
2022-04-30 09:46:03 +0700

    updata 002

--- solitaire/sw.js
+++ solitaire/sw.js
@@ -1,28 +1,25 @@
-const staticCacheName = 's-app-v1';
-const assetUrls = [
-  'index.html',
-  '/css/app.min.css',
-  '/js/main.min.js'
-]
+const staticCacheName = "s-app-v1";
+const assetUrls = ["index.html", "css/app.min.css", "js/main.min.js"];
 
-self.addEventListener('install',async () => {
+self.addEventListener("install", async () => {
   const cache = await caches.open(staticCacheName);
   await cache.addAll(assetUrls);
 });
 
-self.addEventListener('activate',async event => {
+self.addEventListener("activate", async event => {
   const cacheNames = await caches.keys();
   await Promise.all(
-    cacheNames.filter(name => name !== staticCacheName)
+    cacheNames
+      .filter(name => name !== staticCacheName)
       .map(name => caches.delete(name))
-  )
+  );
 });
 
-self.addEventListener('fetch',event => {
+self.addEventListener("fetch", event => {
   event.respondWith(cacheFirst(event.request));
 });
 
 async function cacheFirst(request) {
   const cached = await caches.match(request);
-  return cached ?? await fetch(request);
+  return cached ?? (await fetch(request));
 }
