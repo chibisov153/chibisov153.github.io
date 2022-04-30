5b758ca8964180cf9b1dd47a06bdeabbb76b443c
Dima Ch <chibisov153@gmail.com>
2022-04-30 09:21:12 +0700

    solitaire 30.04.22

---
+++ solitaire/sw.js
@@ -0,0 +1,28 @@
+const staticCacheName = 's-app-v1';
+const assetUrls = [
+  'index.html',
+  '/css/app.min.css',
+  '/js/main.min.js'
+]
+
+self.addEventListener('install',async () => {
+  const cache = await caches.open(staticCacheName);
+  await cache.addAll(assetUrls);
+});
+
+self.addEventListener('activate',async event => {
+  const cacheNames = await caches.keys();
+  await Promise.all(
+    cacheNames.filter(name => name !== staticCacheName)
+      .map(name => caches.delete(name))
+  )
+});
+
+self.addEventListener('fetch',event => {
+  event.respondWith(cacheFirst(event.request));
+});
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request);
+  return cached ?? await fetch(request);
+}
